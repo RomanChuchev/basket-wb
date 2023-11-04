98399d09bda5c1d231667fd9a08f5cabdb4ff354
romanchuchev <romanok20@gmail.com>
2023-11-04 19:35:24 +0300

    feat: add header

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('test');
+/*Burger Menu*/
+document
+	.querySelector('.burger-wrapper')
+	.addEventListener('click', function () {
+		document.querySelector('.header__burger').classList.toggle('open');
+		document.querySelector('.header__menu').classList.toggle('open');
+	});
+
+document.querySelector('body').addEventListener('click', function (e) {
+	if (
+		e.target.classList.value !== 'header__burger header-burger open' &&
+		document.querySelector('.header__burger').classList.contains('open')
+	) {
+		document.querySelector('.header__burger').classList.remove('open');
+		document.querySelector('.header__menu').classList.remove('open');
+	}
+});
